1f96d03356857486866c3db4ebdd355aa3bdac90
Dilovan Matini <dilovan@lelav.com>
2021-11-28 16:45:53 +0300

    Today's update

--- main.js
+++ main.js
@@ -12,22 +12,32 @@ window.onload = function(){
         // getting salary category from user selection
         var salary_category = ConvertToNumber(document.getElementById('salary').value);
 
+        // getting advance payment from user input
+        var advance_payment = ConvertToNumber(document.getElementById('advance').value);
+
         var principal_percentage; // the bank will pay for the house based on salary
 
-        if( salary_category == 1 ){
+        if( salary_category == 1 ){ // if salary less than 30,000
+            document.write("Unfortunately, you are not eligible to receive the mortgage because your salary is less than the 30,000.");
+        }
+        else if( salary_category == 30 ){ // if salary between 30,000 - 44,999
             principal_percentage = 25/100;
         }
-        else if( salary_category == 2 ){
+        else if( salary_category == 45 ){ // if salary between 45,000 - 54,999
             principal_percentage = 30/100;
         }
-        else if( salary_category == 3 ){
+        else if( salary_category == 55 ){ // if salary between 55,000 - 69,999
             principal_percentage = 35/100;
         }
-        else if( salary_category == 4 ){
+        else if( salary_category == 70 ){ // if salary greater than or equal to 70,000
             principal_percentage = 45/100;
         }
         else{
-            document.write("Unfortunately, you are not eligible to receive the mortgage because your salary is less than the 30,000.");
+            document.write("Please, select your yearly salary from the drop down list.");
+        }
+
+        if( advance_payment / ( house_price / 100 ) < 20 ){
+            document.write("Unfortunately, you are not eligible to receive the mortgage because the advance payment is less than 20% of the house price.");
         }
 
         var principal = principal_percentage * house_price;
